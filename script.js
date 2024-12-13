c99c8541c3f831364425500d6a0611f2010f665d
Oladaride <olalabi0407@gmail.com>
2024-12-13 13:53:01 +0100

    sub-2

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let firstName=document.getElementById("first-name")
 
-firstName.value="Emmanuel";
+
+let claimButton=document.getElementById("claim");
+
+document.getElementById("claim").addEventListener("click",
+    ()=>{
+        
+        if(claimButton.innerHTML==="CLAIM YOUR TRIAL"){
+            claimButton.innerHTML="CLAIMED. THANK YOU !"
+        }else{
+            claimButton.innerHTML="CLAIM YOUR TRIAL"
+        }
+
+    }
+    
+)
